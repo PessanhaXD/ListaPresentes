2bef120c0339d9c4958f4164e3f22307c6a7f5e5
Rafael Pessanha <rafaelpessanha1611@gmail.com>
2026-06-08 12:57:28 -0300

    Corração do envio de mensagens

--- site/src/services/payments.js
+++ site/src/services/payments.js
@@ -1,12 +1,18 @@
 import axios from "axios";
 
-export async function create_payment(gift_ids, payer_name, payer_whatsapp) {
+export async function create_payment(
+  gift_ids,
+  payer_name,
+  payer_whatsapp,
+  payer_message,
+) {
   const response = await axios.post(
     `${import.meta.env.VITE_API_URL}/payments/create`,
     {
       gift_ids,
       payer_name,
       payer_whatsapp,
+      payer_message,
     },
   );
 
